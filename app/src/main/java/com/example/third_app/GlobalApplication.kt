3792311396b85b63b13c66d08cd47bdcb4852211
yena777 <parkyena01@sookmyung.ac.kr>
2022-07-15 03:54:47 +0900

    kakao key 보안

--- app/src/main/java/com/example/third_app/GlobalApplication.kt
+++ app/src/main/java/com/example/third_app/GlobalApplication.kt
@@ -6,6 +6,6 @@ class GlobalApplication : Application() {
     override fun onCreate(){
         super.onCreate()
         // kakao sdk 초기화
-        KakaoSdk.init(this, "0e5adec60ff5a89f23d916917c8eb7d1")
+        KakaoSdk.init(this, getString(R.string.kakao_application_key))
     }
 }
