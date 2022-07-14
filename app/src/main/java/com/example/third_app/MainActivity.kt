c84dda683101df33975208330e2f40bd426f98b5
yena777 <parkyena01@sookmyung.ac.kr>
2022-07-15 00:35:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/third_app/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.third_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // 하단 탭이 눌렸을 때 화면을 전환하기 위해선 이벤트 처리하기 위해 BottomNavigationView 객체 생성
+        var bnv_main = findViewById(R.id.bnv_main) as BottomNavigationView
+        // OnNavigationItemSelectedListener를 통해 탭 아이템 선택 시 이벤트를 처리
+        // navi_menu.xml 에서 설정했던 각 아이템들의 id를 통해 알맞은 프래그먼트로 변경하게 한다.
+        bnv_main.run {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.main_home -> {
+                        // 다른 프래그먼트 화면으로 이동하는 기능
+                        val homeFragment = HomeFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fl_container, homeFragment).commit()
+                    }
+                    R.id.main_category -> {
+                        val categoryFragment = CategoryFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fl_container, categoryFragment).commit()
+                    }
+                    R.id.main_heart-> {
+                        val heartFragment = HeartFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fl_container, heartFragment).commit()
+                    }
+                    R.id.main_user-> {
+                        val userFragment = UserFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fl_container, userFragment).commit()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.main_home
+        }
+    }
+
+}
