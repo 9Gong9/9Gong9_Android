3607f9e7af0abd40c20332251a8b1118b55c0c75
yena777 <parkyena01@sookmyung.ac.kr>
2022-07-15 23:17:05 +0900

    로그인 backend 연결

---
+++ app/src/main/java/com/example/third_app/Login.kt
@@ -0,0 +1,7 @@
+package com.example.third_app
+
+data class Login(
+    val `data`: Data,
+    val statusCode: Int,
+    val statusMsg: String
+)
--- app/src/main/java/com/example/third_app/LoginActivity.kt
+++ app/src/main/java/com/example/third_app/LoginActivity.kt
@@ -5,10 +5,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.third_app.databinding.ActivityLoginBinding
 import com.kakao.sdk.user.UserApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class LoginActivity : AppCompatActivity() {
+    var login:Login? = null
     private var mBinding: ActivityLoginBinding ?= null
     private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +25,9 @@ class LoginActivity : AppCompatActivity() {
         mBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLoginLogin.setOnClickListener{
+        val intent = Intent(this, MainActivity::class.java)
+
+        binding.btnKakaoLogin.setOnClickListener{
             // 카카오계정으로 로그인
             UserApiClient.instance.loginWithKakaoAccount(this) { token, error ->
                 if (error != null) {
@@ -24,7 +35,6 @@ class LoginActivity : AppCompatActivity() {
                 }
                 else if (token != null) {
                     Log.i(TAG, "로그인 성공 ${token.accessToken}")
-                    val intent = Intent(this, MainActivity::class.java)
                     intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
                     startActivity(intent)
                     finish()
@@ -32,6 +42,56 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
+        // 레트로핏 객체 생성.
+        var retrofit = Retrofit.Builder()
+            .baseUrl("http://192.249.18.195:80")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        // 로그인
+        binding.btnLoginLogin.setOnClickListener{
+            //로그인 서비스 올리기
+            var loginService: LoginService = retrofit.create(LoginService::class.java)
+            var id = binding.etId.text.toString().trim()
+            var password = binding.etPassword.text.toString().trim()
+            loginService.requestLogin(id, password).enqueue(object: Callback<Login> {
+                override fun onFailure(call: Call<Login>, t: Throwable) {
+                    Log.e("Login", "error : 로그인 실패")
+                    var dialog = AlertDialog.Builder(this@LoginActivity)
+                    dialog.setTitle("Error")
+                    dialog.setMessage("로그인 호출을 실패했습니다.")
+                    dialog.show()
+                    //Toast.makeText(applicationContext,"error : 로그인 실패", Toast.LENGTH_SHORT)
+                }
+                override fun onResponse(call: Call<Login>, response: Response<Login>) {
+                    login = response.body()
+                    Log.d("Login:", "statusCode : "+login?.statusCode.toString())
+                    Log.d("Login:", "Msg : "+login?.statusMsg.toString())
+
+                    Toast.makeText(applicationContext, login?.statusMsg.toString(), Toast.LENGTH_SHORT)
+                    if(login?.statusCode.toString()=="201"){
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                        startActivity(intent)
+                        finish()
+                    }
+                    else{
+                        var dialog = AlertDialog.Builder(this@LoginActivity)
+                        dialog.setTitle("Error "+login?.statusCode.toString())
+                        dialog.setMessage(login?.statusMsg.toString())
+                        dialog.show()
+                    }
+                }
+            })
+        }
+
+        // 회원가입
+        binding.btnSignUp.setOnClickListener{
+            //Toast.makeText(this, "토스트 메시지", Toast.LENGTH_SHORT)
+            val intent = Intent(this, SignUpActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }
     }
     // 액티비티가 파괴될 때..
     override fun onDestroy() {
