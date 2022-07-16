14ff4b164dee4696c95752f09324f4e974d4a143
jina <jk020218@gmail.com>
2022-07-16 21:43:57 +0900

    before starting logout

--- app/src/main/java/com/example/third_app/Login.kt
+++ app/src/main/java/com/example/third_app/Login.kt
@@ -1,6 +1,6 @@
 package com.example.third_app
 
-data class Login(
+data class Login( //OUTPUT 서비스
     val `data`: LoginData,
     val statusCode: Int,
     val statusMsg: String
--- app/src/main/java/com/example/third_app/LoginService.kt
+++ app/src/main/java/com/example/third_app/LoginService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.PUT
 
-interface LoginService {
+interface LoginService { //INPUT 서비스
     @FormUrlEncoded //인코딩 옵션
     @PUT("/user") //어떤 형태로 데이터를 전송할 것인가
     fun requestLogin(
