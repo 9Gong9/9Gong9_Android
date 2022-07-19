9f422aa7df0cfe7b51de3d498a23e096ad558289
yena777 <parkyena01@sookmyung.ac.kr>
2022-07-19 16:28:09 +0900

    conflict error 해결

--- app/src/main/java/com/example/third_app/heart/HeartData.kt
+++ app/src/main/java/com/example/third_app/heart/HeartData.kt
@@ -1,8 +1,5 @@
 package com.example.third_app.heart
 import com.example.third_app.home.Product
-
-import com.example.third_app.home.Product
-
 data class HeartData (
     val usersLikedItem: List<Product>?
         )
