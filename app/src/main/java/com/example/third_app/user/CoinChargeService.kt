20002898d330c9ac5c93205642ccacc6cb6fe5cd
yena777 <parkyena01@sookmyung.ac.kr>
2022-07-21 02:04:17 +0900

    자잘한 것들 수정2

--- app/src/main/java/com/example/third_app/user/CoinChargeService.kt
+++ app/src/main/java/com/example/third_app/user/CoinChargeService.kt
@@ -6,11 +6,11 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface CoinChargeService {
-//    @FormUrlEncoded
+    @FormUrlEncoded
     @PUT("/user/{userId}/budget") //어떤 형태로 데이터를 전송할 것인가
     fun requestCoin(
         @Path("userId",encoded=true) id:String, //id String인 이유?
 //        @Field("budget") budget : String
-        @Query("newValue") newValue: Int
+        @Field("newValue") newValue: Int
     ) : Call<CoinCharge> // 어떤 형태로 데이터를 받을 것인가 res
 }
